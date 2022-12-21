f40834482605b2f075b081ea48e95cb0d61d133e
Beni-Kipaka <benikipaka01@gmail.com>
2022-12-21 10:07:54 +0100

    first commit

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,10 @@
+// INITIALIZE SWIPER
+var swiper = new Swiper(".swiper-container", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    }
+});
